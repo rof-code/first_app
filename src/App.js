a19751dfd304cb00818976a68b28247bd81def8a
Rof <rof.bra@gmail.com>
2021-08-16 12:18:19 -0300

    First App with constructor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.handlePClick = this.handlePClick.bind(this);
+
+    this.state = {
+      name: 'Rodolfo Augusto',
+      counter: 0
+    };
+  }
+
+  handlePClick(){
+    this.setState({name: 'Melissa'});
+  }
+  handeAClick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({counter: counter + 1});
+  }
+  render(){
+  //Sem destruct
+  // const name = this.state.name;
+
+  // Com destruct
+  const {name, counter} = this.state;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={this.handlePClick}>
+          {name} {counter}
         </p>
         <a
+          onClick={this.handeAClick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -20,6 +47,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
